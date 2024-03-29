a2eaf46d7e2ef70a0b056d9a74116d1629a1a05c
nikhiltien <nikhil17tn@gmail.com>
2024-03-29 15:08:12 -0400

    .gitignore

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
-class App:
-	def test(self):
-		print('Hello, world!')
+from src.logger import setup_logger
+
+if __name__ == "__main__":
+    logging = setup_logger(level='INFO', stream=False)
+    logging.info("Hello, world!")
