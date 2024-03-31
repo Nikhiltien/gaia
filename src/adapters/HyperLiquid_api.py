eafe0838a2aeb424581bad292c53f23390a816da
nikhiltien <nikhil17tn@gmail.com>
2024-03-31 01:57:00 -0400

    method parsing

--- src/adapters/HyperLiquid_api.py
+++ src/adapters/HyperLiquid_api.py
@@ -3,6 +3,7 @@ import random
 import asyncio
 import logging
 
+from typing import List, Dict
 from base_adapter import Adapter
 from ws_client import WebsocketClient
 
@@ -28,6 +29,7 @@ class HyperLiquid(WebsocketClient, Adapter):
     async def connect(self):
         await super()._connect()
         self.start_msg_loop()
+        self.logger.info(f"Connected to HyperLiquid.")
 
     def _handle_incoming_message(self, message):
         channel = message.get('channel')
@@ -69,7 +71,7 @@ class HyperLiquid(WebsocketClient, Adapter):
             self._process_order_book(message)
         elif "trade" in channel:
             self._process_trade(message)
-        elif "orderUpdate" in channel:
+        elif "orderUpdates" in channel:
             self._process_orders(message)
         else:
             self.logger.info(f"Unrecognized channel: {message}")
@@ -151,23 +153,105 @@ class HyperLiquid(WebsocketClient, Adapter):
         return await self._subscribe_to_topic(method="subscribe", params=params, req_id=req_id)
 
     def _process_notification(self, data):
-        # Process and log notification data
-        print(f"Notification: {data}")
+        notification = data.get("notification")
+        print(f"Notification: {notification}")
 
     def _process_user_event(self, data):
-        # Process user event data
-        print(f"User Event: {data}")
+        parsed_events = []
+        if 'fills' in data:
+            for fill in data['fills']:
+                parsed_event = {
+                    "eventType": "Fill",
+                    "coin": fill.get('coin'),
+                    "price": fill.get('px'),
+                    "size": fill.get('sz'),
+                    "side": fill.get('side'),
+                    "time": fill.get('time'),
+                    "startPosition": fill.get('startPosition'),
+                    "direction": fill.get('dir'),
+                    "closedPnL": fill.get('closedPnl'),
+                    "hash": fill.get('hash'),
+                    "orderID": fill.get('oid'),
+                    "crossed": fill.get('crossed'),
+                    "fee": fill.get('fee'),
+                    "tradeID": fill.get('tid'),
+                    "feeToken": fill.get('feeToken')
+                }
+                parsed_events.append(parsed_event)
+
+        if 'liquidation' in data:
+            liquidation = data['liquidation']
+            parsed_event = {
+                "eventType": "Liquidation",
+                "liquidationID": liquidation.get('lid'),
+                "liquidator": liquidation.get('liquidator'),
+                "liquidatedUser": liquidation.get('liquidated_user'),
+                "netLossPosition": liquidation.get('liquidated_ntl_pos'),
+                "accountValue": liquidation.get('liquidated_account_value')
+            }
+            parsed_events.append(parsed_event)
+
+        if 'nonUserCancel' in data:
+            for cancel in data['nonUserCancel']:
+                parsed_event = {
+                    "eventType": "NonUserCancel",
+                    "coin": cancel.get('coin'),
+                    "orderID": cancel.get('oid')
+                }
+                parsed_events.append(parsed_event)
+
+        print(f"User Events: {parsed_events}")
 
     def _process_orders(self, data):
-        # Process user event data
-        print(f"orderEvent: {data}")
+        order_updates = data.get('data', [])
+        parsed_orders = []
+        for order_data in order_updates:  # Iterating through the list of order updates
+            order = order_data.get('order', {})
+            parsed_order = {
+                "coin": order.get('coin'),
+                "side": order.get('side') == 'B' and "buy" or "sell",
+                "limitPrice": order.get('limitPx'),
+                "size": order.get('sz'),
+                "orderID": order.get('oid'),
+                "timestamp": order.get('timestamp'),
+                "originalSize": order.get('origSz'),
+                "reduceOnly": order.get('reduceOnly', False),
+                "status": order_data.get('status'),
+                "statusTimestamp": order_data.get('statusTimestamp')
+            }
+            parsed_orders.append(parsed_order)
+        print(f"Order Events: {parsed_orders}")
 
     def _process_order_book(self, data):
-        book = data.get("data")
-        print(f"Book: {book}")
+        book = data.get("data", {}).get("levels", [])
+        if len(book) == 2:
+            bids = book[0]
+            asks = book[1]
 
-    def _process_trade(self, data):
-        trades = data.get("data")
+            parsed_book = {
+                "timestamp": data.get("data", {}).get("time"),
+                "bids": [{"price": level.get("px"), "qty": level.get("sz")} for level in bids], # "n": level.get("n")
+                "asks": [{"price": level.get("px"), "qty": level.get("sz")} for level in asks] # "n": level.get("n")
+            }
+        else:
+            parsed_book = {
+                "error": "Invalid book structure",
+            }
+
+        print(f"Order Book: {parsed_book}")
+
+    def _process_trade(self, data: List[Dict]) -> List[Dict]:
+        trades = []
+        for trade in data.get("data"):
+            trades.append({
+                "coin": trade["coin"],
+                "side": trade["side"],
+                "price": float(trade["px"]),
+                "size": float(trade["sz"]),
+                "time": trade["time"],
+                "hash": trade["hash"],
+                "trade_id": trade["tid"]
+            })
         print(f"Trades: {trades}")
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -176,11 +260,11 @@ eth = {
     "symbol": "ETH",
 }
 
+wallet = '0x7195d5fBC22Afa1FF6A0A25591285Db7a81838D4'
+
 async def main():
     adapter = HyperLiquid()
     await adapter.connect()
-
-    wallet = '0x7195d5fBC22Afa1FF6A0A25591285Db7a81838D4'
 
     await adapter.subscribe_notifications(user_address=wallet)
     await adapter.subscribe_user_events(user_address=wallet)
